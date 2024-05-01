04b811cdea78aa9e0cbb84d22c9b11471a491175
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2024-05-01 14:48:17 -0300

    update sorts

--- sorting/selection-sort.c
+++ sorting/selection-sort.c
@@ -1,3 +1,15 @@
 #include "item.h"
 
-void sort(Item *a, int lo, int hi) {}
+void sort(Item *a, int lo, int hi) {
+    for (int i = lo; i <= hi; i++) {
+        Item least = a[i];
+        int index = i;
+        for (int j = i + 1; j <= hi; j++) {
+            if (less(a[j], least)) {
+                least = a[j];
+                index = j;
+            }
+        }
+        exch(a[index], a[i]);
+    }
+}
--- sorting/shaker-sort.c
+++ sorting/shaker-sort.c
@@ -1,3 +1,12 @@
 #include "item.h"
 
-void sort(Item *a, int lo, int hi) {}
+void sort(Item *a, int lo, int hi) {
+    for (int i = lo; i <= hi; i++) {
+        for (int j = 0; j < hi - i; j++) {
+            compexch(a[j], a[j + 1]);
+        }
+        for (int j = hi - i - 1; j >= i; j--) {
+            compexch(a[j - 1], a[j]);
+        }
+    }
+}
